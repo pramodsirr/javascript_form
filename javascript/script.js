989f912e1f52df41b68b8e1bba5b1eb7d8ef497b
pramodsirr <herepramod@gmail.com>
2020-05-05 06:49:10 -0400

    javascript_form

--- javascript/script.js
+++ javascript/script.js
@@ -3,5 +3,35 @@ function showDate() {
   document.getElementById("datep").innerHTML = Date();
 }
 
-	     
+
+function myFunction() {
+
+  alert("The form was submitted");
+}
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+  }
+}
+function test() {
+  var x = document.getElementById("confirm_password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+	function test1() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 	
